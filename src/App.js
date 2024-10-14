d1d6ca109feda455fb5623b3fc5c7aeb64a8d0fe
Michał Wojciechowski <michalwojogolne@gmail.com>
2024-10-14 19:30:30 +0200

    Lab01

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { data } from "./module-data";
+import CarProfile from "./components/CarProfile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Car List</h1>
+        {data.map((car) => (
+          <CarProfile key={car.id} car={car} />
+        ))}
       </header>
     </div>
   );
---
+++ src/module-data.js
@@ -0,0 +1,12 @@
+export const data = [
+  {"id":1,"brand":"Ford","productionDate":"1995-06-05","plate":"SWM27887","color":"blue"},
+  {"id":2,"brand":"Volkswagen","productionDate":"1993-11-21","plate":"ENA91323","color":"black"},
+  {"id":3,"brand":"Toyota","productionDate":"2009-02-25","plate":"OMC97485","color":"red"},
+  {"id":4,"brand":"Volkswagen","productionDate":"2021-09-13","plate":"AOB43922","color":"red"},
+  {"id":5,"brand":"Audi","productionDate":"2020-07-26","plate":"MIM85165","color":"blue"},
+  {"id":6,"brand":"BMW","productionDate":"1991-11-08","plate":"ZXJ20568","color":"red"},
+  {"id":7,"brand":"Ford","productionDate":"2004-12-19","plate":"RKE07490","color":"black"},
+  {"id":8,"brand":"BMW","productionDate":"2022-05-13","plate":"HDX58432","color":"blue"},
+  {"id":9,"brand":"Ford","productionDate":"1995-03-23","plate":"MJM71948","color":"red"},
+  {"id":10,"brand":"Audi","productionDate":"1994-01-16","plate":"BLR05712","color":"red"},
+];
